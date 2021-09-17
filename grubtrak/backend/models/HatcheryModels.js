10788a8b27afcc62d3178d760e20c7d1a48d9172
jadenwest <jade234876@gmail.com>
2021-09-17 13:07:41 -0400

    added hatchery model in backend

---
+++ grubtrak/backend/models/HatcheryModels.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const hatcheryTemplate = new mongoose.Schema({
+    hatcheryID: {
+        type: String,
+        required: true 
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    dimensions: { 
+        //pass in single number of L,W,H, as 2-digit values: 001122, parse for every 2 digits
+        type: Number,
+        required: false //need to change this and below to true once hatchery input data on front end is made 
+    },
+    grubCount: {
+        type: Number,
+        required: false
+    },
+    density: {
+        type: Number,
+        required: false
+    },
+    feedType: {
+        type: String,
+        required: false
+    },
+    feedWeight: {
+        type: Number,
+        required: false
+    },
+    substrateType: {
+        type: String,
+        required: false
+    },
+    substrateWeight: {
+        type: Number,
+        required: false,
+    }
+})
