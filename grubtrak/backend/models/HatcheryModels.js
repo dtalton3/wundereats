5dc46bc1bc3b697012ea284aa1582f01619d2950
jadenwest <jade234876@gmail.com>
2021-09-28 16:17:13 -0400

    comment out unrequired HatcheryModel fields

--- grubtrak/backend/models/HatcheryModels.js
+++ grubtrak/backend/models/HatcheryModels.js
@@ -9,35 +9,35 @@ const hatcheryTemplate = new mongoose.Schema({
         type: String,
         required: true
     },
-    dimensions: { 
-        //pass in single number of L,W,H, as 2-digit values: 001122, parse for every 2 digits
-        type: Number,
-        required: false //need to change this and below to true once hatchery input data on front end is made 
-    },
-    grubCount: {
-        type: Number,
-        required: false
-    },
-    density: {
-        type: Number,
-        required: false
-    },
-    feedType: {
-        type: String,
-        required: false
-    },
-    feedWeight: {
-        type: Number,
-        required: false
-    },
-    substrateType: {
-        type: String,
-        required: false
-    },
-    substrateWeight: {
-        type: Number,
-        required: false,
-    }
+    // dimensions: { 
+    //     //pass in single number of L,W,H, as 2-digit values: 001122, parse for every 2 digits
+    //     type: Number,
+    //     required: false //need to change this and below to true once hatchery input data on front end is made 
+    // },
+    // grubCount: {
+    //     type: Number,
+    //     required: false
+    // },
+    // density: {
+    //     type: Number,
+    //     required: false
+    // },
+    // feedType: {
+    //     type: String,
+    //     required: false
+    // },
+    // feedWeight: {
+    //     type: Number,
+    //     required: false
+    // },
+    // substrateType: {
+    //     type: String,
+    //     required: false
+    // },
+    // substrateWeight: {
+    //     type: Number,
+    //     required: false,
+    // }
 })
 
 module.exports = mongoose.model('Users', hatcheryTemplate)
