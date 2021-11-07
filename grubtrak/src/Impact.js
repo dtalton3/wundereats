a98c48121c1b6aba337ae9869483b4c0b6063693
Hung <immortalnoob436436@gmail.com>
2021-11-07 14:26:04 -0500

    Impact Report button

--- grubtrak/src/Impact.js
+++ grubtrak/src/Impact.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Home.css";
+import "./Impact.css";
 import Navbar from './components/Navbar/Navbar';
 import VNavbar from './components/VNavbar/VNavbar';
 import GlobalStyle from "./globalStyles";
-
+import larvae from "./images/larvae2.png";
 
 function Impact() {
     // function backToHome(event) {
@@ -26,14 +26,19 @@ function Impact() {
             <div className="Mid-panel">
                 <h1 className='Mid-panel-content'>Impact Reports</h1>
             </div>
-
-            {/* <div className="Dashboard">
-                <input type = 'submit'
-                        className='btn btn-danger btn-block'
-                        value='Login'
-                        onClick={backToHome}>
-                </input>
-            </div> */}
+            <div className="Dashboard">
+                <div className="Hatchery">Hatchery #1
+                    <div className="HatcheryDetails">
+                        <div className="HatcherySpecs">
+                            <img src={larvae} className="larvae-small-icon" alt="Larvae"/>
+                            <div>Larvae Count: 1000</div>
+                            <div>Hatchery Mass: _ kg</div>
+                        </div>
+                        <button className="generate">Generate Impact Report</button>
+                    </div>
+                    
+                </div>
+            </div>
             <GlobalStyle />
         </div>
     );
